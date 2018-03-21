65997bdfbd505a7e5cd7c41203fa1e9988c388dd
Indra Gunanda <indra.gunanda@gmail.com>
2018-03-21 12:50:02 +0700

    Init Home

---
+++ application/views/public/home.php
@@ -0,0 +1,427 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!-- ini bagian events dan blog -->
+<div class="container">
+  <div class="row post-section">
+    <div class="col-md-4 col-xs-12 wow fadeIn" data-wow-delay=".1s">
+      <!-- section events -->
+      <aside class="sidebar-left" id="widget-event">
+        <div class="widget-header">
+          <img src="assets/image/kal.png" width="50">
+          <h4 class="widget-header-title">Events</h4>
+        </div>
+        <div class="widget-content">
+          <ul class="events-list">
+            <li><a href="">Seminar cara membangun startup digital dengan Dalih Rusmana</a> <br>
+              <span class="event-date">Kamis 20 Maret 2018</span> <br>
+              <span class="event-place">Auditorium Unikom Bandung</span>
+            </li>
+            <li><a href="">Workshop digital forensic bandung developer</a> <br>
+              <span class="event-date">Selasa 22 Agustus 2018</span> <br>
+              <span class="event-place">Lt. 7010 Gedung Baru</span>
+            </li>
+            <li><a href="">Information System Cup 2018 Hima Si</a> <br>
+              <span class="event-date">Jum'at s.d Minggu 15 Maret 2018</span> <br>
+              <span class="event-place">Lapangan Futsal</span>
+            </li>
+          </ul>
+        </div>
+        <div class="event-more">
+          <a href="events-page.html">Lihat Semua >></a>
+        </div>
+      </aside> <!-- end events -->
+
+      <!-- ini bagian section pengunjung -->
+      <aside class="sidebar-left" id="widget-event">
+        <div class="widget-header">
+          <img src="assets/image/bar-chart.png" width="50">
+          <h4 class="widget-header-title">Pengunjung</h4>
+        </div>
+        <div class="widget-content">
+          <ul class="visited">
+            <li>215 views </li>
+          </ul>
+        </div>
+      </aside> <!-- end pengunjung -->
+    </div>
+
+    <!-- ini bagian future blog post -->
+    <div class="col-md-8 col-xs-12 blog-post">
+      <div class="col-md-12">
+        <div class="col-md-6 blog-post-inner wow fadeIn" data-wow-delay=".5s">
+          <div class="thumbnail">
+            <a href="#"><img src="assets/image/image-1.jpeg" alt="dalih" class="blog-image"></a>
+            <div class="caption">
+              <a href=""><span class="label" style="background-color: #13CE66; border-radius: 0; border-bottom-right-radius: .50em; padding: 3px 8px;">Pendidikan</span></a> <br><br>
+                  <a href="#" class="blog-title">Cara Meningkatkan Motivasi dalam Berorganisasi</a>
+                  <div class="tgl-posting">
+                    <div class="date">
+                      <span>22 Juni, 2018</span>
+                    </div>
+                    <div class="view">
+                      <span><i class="fa fa-eye"></i>
+                      187</span>
+                    </div>
+                  </div>
+                  <div class="post-footer">
+                    <div class="photo-admin">
+                      <img src="assets/image/dalih.jpg" class="img-admin" width="15" height="15">
+                    </div>
+                    <div class="nama-admin">
+                      <a href="#" class="name">Dalih Rusmana</a>
+                    </div>
+                  </div>
+                </div>
+
+          </div>
+        </div>
+        <div class="col-md-6 blog-post-inner wow fadeIn" data-wow-delay="1s">
+          <div class="thumbnail">
+            <a href="#"><img src="assets/image/image-2.jpg" alt="dalih" class="blog-image"></a>
+            <div class="caption">
+              <a href=""><span class="label" style="background-color: #13CE66; border-radius: 0; border-bottom-right-radius: .50em; padding: 3px 8px;">Pendidikan</span></a> <br><br>
+                  <a href="#" class="blog-title">Cara Meningkatkan Motivasi dalam Berorganisasi</a>
+                  <div class="tgl-posting">
+                    <div class="date">
+                      <span>22 Juni, 2018</span>
+                    </div>
+                    <div class="view">
+                      <span><i class="fa fa-eye"></i>
+                      187</span>
+                    </div>
+                  </div>
+                  <div class="post-footer">
+                    <div class="photo-admin">
+                      <img src="assets/image/dalih.jpg" class="img-admin" width="15" height="15">
+                    </div>
+                    <div class="nama-admin">
+                      <a href="#" class="name">Dalih Rusmana</a>
+                    </div>
+                  </div>
+                </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="col-md-4 blog-post-inner wow fadeIn" data-wow-delay=".3s">
+          <div class="thumbnail">
+            <a href="#"><img src="assets/image/image-3.jpeg" alt="dalih" class="blog-image"></a>
+            <div class="caption">
+              <a href=""><span class="label" style="background-color: #13CE66; border-radius: 0; border-bottom-right-radius: .50em; padding: 3px 8px;">Pendidikan</span></a> <br><br>
+                  <a href="#" class="blog-title">Cara Meningkatkan Motivasi dalam Berorganisasi</a>
+                  <div class="tgl-posting">
+                    <div class="date">
+                      <span>22 Juni, 2018</span>
+                    </div>
+                    <div class="view">
+                      <span><i class="fa fa-eye"></i>
+                      187</span>
+                    </div>
+                  </div>
+                  <div class="post-footer">
+                    <div class="photo-admin">
+                      <img src="assets/image/dalih.jpg" class="img-admin" width="15" height="15">
+                    </div>
+                    <div class="nama-admin">
+                      <a href="#" class="name">Dalih Rusmana</a>
+                    </div>
+                  </div>
+                </div>
+
+          </div>
+        </div>
+        <div class="col-md-4 blog-post-inner wow fadeIn" data-wow-delay=".5s">
+          <div class="thumbnail">
+            <a href="#"><img src="assets/image/image-4.jpeg" alt="dalih" class="blog-image"></a>
+            <div class="caption">
+              <a href=""><span class="label" style="background-color: #13CE66; border-radius: 0; border-bottom-right-radius: .50em; padding: 3px 8px;">Pendidikan</span></a> <br><br>
+                  <a href="#" class="blog-title">Cara Meningkatkan Motivasi dalam Berorganisasi</a>
+                  <div class="tgl-posting">
+                    <div class="date">
+                      <span>22 Juni, 2018</span>
+                    </div>
+                    <div class="view">
+                      <span><i class="fa fa-eye"></i>
+                      187</span>
+                    </div>
+                  </div>
+                  <div class="post-footer">
+                    <div class="photo-admin">
+                      <img src="assets/image/dalih.jpg" class="img-admin" width="15" height="15">
+                    </div>
+                    <div class="nama-admin">
+                      <a href="#" class="name">Dalih Rusmana</a>
+                    </div>
+                  </div>
+                </div>
+
+          </div>
+        </div>
+        <div class="col-md-4 blog-post-inner wow fadeIn" data-wow-delay=".4s">
+          <div class="thumbnail">
+            <a href="#"><img src="assets/image/image-5.jpeg" alt="dalih" class="blog-image"></a>
+            <div class="caption">
+              <a href=""><span class="label" style="background-color: #13CE66; border-radius: 0; border-bottom-right-radius: .50em; padding: 3px 8px;">Pendidikan</span></a> <br><br>
+                  <a href="#" class="blog-title">Cara Meningkatkan Motivasi dalam Berorganisasi</a>
+                  <div class="tgl-posting">
+                    <div class="date">
+                      <span>22 Juni, 2018</span>
+                    </div>
+                    <div class="view">
+                      <span><i class="fa fa-eye"></i>
+                      187</span>
+                    </div>
+                  </div>
+                  <div class="post-footer">
+                    <div class="photo-admin">
+                      <img src="assets/image/dalih.jpg" class="img-admin" width="15" height="15">
+                    </div>
+                    <div class="nama-admin">
+                      <a href="#" class="name">Dalih Rusmana</a>
+                    </div>
+                  </div>
+                </div>
+
+          </div>
+        </div>
+      </div>
+    </div> <!-- end bagian future blog post -->
+  </div>
+</div>
+
+<!-- ini bagian section gallery kang -->
+<div id="gallery-section">
+  <div class="image-gradient">
+  <div class="container">
+    <div class="section-judul wow fadeIn">
+      <h2>Gallery <br>HIMA SI</h2>
+      <span class="garis"></span>
+      <p>Kami bangga pada hima si karena di situlah jiwa kami memupuk kami dan setia sama kami ininlah jiwa dan raga kami untuk indonesia yang beragama dan ikhlas menjalaninya</p>
+    </div>
+  </div>
+  <div class="row gallery-content gallery wow fadeIn">
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <figure class="col-md-2 effect-ming">
+      <a href="assets/image/dalih.jpg" class="thumbnail" data-caption="dalih rusmana yang ganteng">
+        <img src="assets/image/dalih.jpg" alt="dalih">
+      </a>
+    </figure>
+    <div class="col-md-2">
+      <div class="thumbnail">
+        <img src="assets/image/dalih.jpg">
+      </div>
+    </div>
+  </div>
+  </div>
+</div> <!-- akhir dari segala gallery wkwkw -->
+
+<!-- ini bagian kesan pesan kang kade ulah poho -->
+<div id="kesanpesan">
+  <div class="image-gradient-dua">
+    <div class="container wow fadeIn">
+      <div class="section-judul">
+        <h2>Kesan Pesan <br>Alumni</h2>
+        <span class="garis"></span>
+        <p>Kami bangga pada hima si karena di situlah jiwa kami memupuk kami dan setia sama kami ininlah jiwa dan raga kami untuk indonesia yang beragama dan ikhlas menjalaninya</p>
+      </div>
+    </div>
+    <div class="pesan-alumni wow fadeIn">
+    <div class="responsive">
+      <div class="kotak-pesan">
+        <ul class="title-pesan">
+            <li class="poto-profil-alumni">
+              <img src="assets/image/dalih.jpg">
+            </li>
+            <li class="profil-alumni">
+              <h4>Firdhan Vandaru</h4>
+              <p>Ketua HIMA 2016</p>
+            </li>
+        </ul>
+        <div class="content-pesan">
+          <p>“ Hidup ini adalah perjuangan dan teruslah berjuang
+kawan - kawan karena hidup di dunia ini tidak sekedar
+hidup, kalo sekedar hidup hewan juga hidup, tapi hidup
+untuk orang orang banyak”</p>
+        </div>
+      </div>
+      <div class="kotak-pesan">
+        <ul class="title-pesan">
+            <li class="poto-profil-alumni">
+              <img src="assets/image/dalih.jpg">
+            </li>
+            <li class="profil-alumni">
+              <h4>Firdhan Vandaru</h4>
+              <p>Ketua HIMA 2016</p>
+            </li>
+        </ul>
+        <div class="content-pesan">
+          <p>“ Hidup ini adalah perjuangan dan teruslah berjuang
+kawan - kawan karena hidup di dunia ini tidak sekedar
+hidup, kalo sekedar hidup hewan juga hidup, tapi hidup
+untuk orang orang banyak”</p>
+        </div>
+      </div>
+      <div class="kotak-pesan">
+        <ul class="title-pesan">
+            <li class="poto-profil-alumni">
+              <img src="assets/image/dalih.jpg">
+            </li>
+            <li class="profil-alumni">
+              <h4>Firdhan Vandaru</h4>
+              <p>Ketua HIMA 2016</p>
+            </li>
+        </ul>
+        <div class="content-pesan">
+          <p>“ Hidup ini adalah perjuangan dan teruslah berjuang
+kawan - kawan karena hidup di dunia ini tidak sekedar
+hidup, kalo sekedar hidup hewan juga hidup, tapi hidup
+untuk orang orang banyak”</p>
+        </div>
+      </div>
+      <div class="kotak-pesan">
+        <ul class="title-pesan">
+            <li class="poto-profil-alumni">
+              <img src="assets/image/dalih.jpg">
+            </li>
+            <li class="profil-alumni">
+              <h4>Firdhan Vandaru</h4>
+              <p>Ketua HIMA 2016</p>
+            </li>
+        </ul>
+        <div class="content-pesan">
+          <p>“ Hidup ini adalah perjuangan dan teruslah berjuang
+kawan - kawan karena hidup di dunia ini tidak sekedar
+hidup, kalo sekedar hidup hewan juga hidup, tapi hidup
+untuk orang orang banyak”</p>
+        </div>
+      </div>
+      <div class="kotak-pesan">
+        <ul class="title-pesan">
+            <li class="poto-profil-alumni">
+              <img src="assets/image/dalih.jpg">
+            </li>
+            <li class="profil-alumni">
+              <h4>Firdhan Vandaru</h4>
+              <p>Ketua HIMA 2016</p>
+            </li>
+        </ul>
+        <div class="content-pesan">
+          <p>“ Hidup ini adalah perjuangan dan teruslah berjuang
+kawan - kawan karena hidup di dunia ini tidak sekedar
+hidup, kalo sekedar hidup hewan juga hidup, tapi hidup
+untuk orang orang banyak”</p>
+        </div>
+      </div>
+      <div class="kotak-pesan">
+        <ul class="title-pesan">
+            <li class="poto-profil-alumni">
+              <img src="assets/image/dalih.jpg">
+            </li>
+            <li class="profil-alumni">
+              <h4>Firdhan Vandaru</h4>
+              <p>Ketua HIMA 2016</p>
+            </li>
+        </ul>
+        <div class="content-pesan">
+          <p>“ Hidup ini adalah perjuangan dan teruslah berjuang
+kawan - kawan karena hidup di dunia ini tidak sekedar
+hidup, kalo sekedar hidup hewan juga hidup, tapi hidup
+untuk orang orang banyak”</p>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="container wow fadeInUp">
+      <div class="section-judul wow fadeIn">
+        <h2>Kesekretariatan <br>HIMA SI</h2>
+        <span class="garis"></span>
+        <p>Kami bangga pada hima si karena di situlah jiwa kami memupuk kami dan setia sama kami ininlah jiwa dan raga kami untuk indonesia yang beragama dan ikhlas menjalaninya</p>
+      </div>
+      <div class="row contact wow fadeIn">
+        <div class="col-md-8">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1400.436337988144!2d107.61439729217302!3d-6.886755553780138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6f8aa08188b%3A0x632d24e6061e8903!2sUniversitas+Komputer+Indonesia!5e0!3m2!1sid!2sid!4v1519800151898" width="500" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-4 contact-info">
+          <h3>Informasi Kontak</h3>
+          <ul>
+            <li>R. 4314 Gedung Miracle <br>
+            Unikom Bandung. 43176</li>
+            <li>+857 - 9316 - 7490</li>
+            <li>himasi@email.unikom.ac.id</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 wow fadeIn">
+          <div class="social-widget">
+            <iframe width="100%" height="200" src="https://www.youtube.com/embed/ANs5DsKMptM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="col-md-4 wow fadeInUp">
+          <div class="social-widget">
+            <iframe width="100%" height="200" src="https://www.youtube.com/embed/tgoJhJbUSv8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="col-md-4 wow fadeIn">
+          <div class="social-widget">
+            <iframe width="100%" height="200" src="https://www.youtube.com/embed/9j27bbHJ7yc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> <!-- end kesan pesan -->
