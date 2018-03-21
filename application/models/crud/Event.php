f2ba26a1337d31403122bb5c32613bebbe7a28cb
Indra Gunanda <indra.gunanda@gmail.com>
2018-03-21 08:55:45 +0700

    [MODEL] Main CRUD

---
+++ application/models/crud/Event.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Event extends CI_Model{
+  public $table = "event";
+  public function __construct()
+  {
+    parent::__construct();
+
+  }
+  public function get($data = "")
+  {
+    if($data != ""){
+      return $this->db->get_where($this->table,$data);
+    }else{
+    return $this->db->get($this->table);
+    }
+  }
+  public function insert($data=[])
+  {
+    return $this->db->insert($this->table,$data);
+  }
+  public function delete($data=[])
+  {
+     $this->db->delete($this->table,$data);
+     return $this->db->affected_rows() > 0;
+  }
+  public function update($data=[],$where=[])
+  {
+    if(count($where) > 0){
+      $this->db->update($this->table,$data,$where);
+      return $this->db->affected_rows() > 0;
+    }else{
+      return false;
+    }
+  }
+
+}
